4c30252744dbb915bc36216da33dae9ec24baa6a
Mathias Göbel <goebel@sub.uni-goettingen.de>
2020-12-07 20:58:12 +0100

    feat: add container log

--- parallelize.sh
+++ parallelize.sh
@@ -11,12 +11,17 @@ WORK_DIR="/tmp/fredracor"
 SOURCE_DIR="$WORK_DIR/data"
 TARGET_DIR="$WORK_DIR/transformed"
 
-START=$(date +"%H:%M:%S")
+START=$(date +"%T")
 echo $START
 
 # create dirs
 if [ ! -d $WORK_DIR ];   then mkdir $WORK_DIR; fi
-if [ ! -d $SOURCE_DIR ]; then mkdir $SOURCE_DIR; fi
+if [ ! -d $SOURCE_DIR ];
+    then mkdir $SOURCE_DIR; 
+    else
+        echo "SOURCE_DIR found with $(ls $SOURCE_DIR | wc -l) items."
+        read -p "Do you want to reload source? [y/n]" reload
+fi
 if [ ! -d $TARGET_DIR ]; then mkdir $TARGET_DIR; fi
 
 for i in $(eval echo {1..$THREADS}); do
@@ -38,7 +43,7 @@ for i in $(eval echo {1..$THREADS}); do
     echo "$i ready."
 done
 
-echo "$(date +"%H:%M:%S") :: ready"
+echo "$(date +"%T") :: ready"
 
 # put scripts to the databases
 for i in $(eval echo {1..$THREADS}); do
@@ -51,14 +56,16 @@ for i in $(eval echo {1..$THREADS}); do
 
 # load data at first instance, shared to all others afterwards
 # about 15 minutes
-echo "load data on instance 1"
-for file in load*.xq; do
-   curl --silent --output report-load.log http://admin:@localhost:8081/exist/rest/db/$file
-done
+if [[ $reload != "n" ]]; then
+    echo "load data on instance 1"
+    for file in load*.xq; do
+        curl --silent --output report-load.log http://admin:@localhost:8081/exist/rest/db/$file
+    done
+fi
 
 echo "load source data to volume, remove from instance 1 afterwards"
 curl "http://admin:@localhost:8081/exist/rest/db/parallelize-download-source.xq"
-echo "$(date +"%H:%M:%S") :: load data"
+echo "$(date +"%T") :: load data"
 
 # distribute source data to the databases
 # about 3 minutes
@@ -79,7 +86,7 @@ for instance in $(eval echo {1..$THREADS}); do
 done
 
 wait
-echo "$(date +"%H:%M:%S") :: distribute"
+echo "$(date +"%T") :: distribute"
 
 # return to git repo dir
 cd $THIS_DIR
@@ -102,7 +109,7 @@ for i in $(eval echo {1..$THREADS}); do
 done
 
 wait
-echo "$(date +"%H:%M:%S") :: transform"
+echo "$(date +"%T") :: transform"
 
 # get the transformed data
 for instance in $(eval echo {1..$THREADS}); do
@@ -114,11 +121,18 @@ wait
 # import new files to THIS repo
 cp $TARGET_DIR/*.xml $THIS_DIR/tei/
 
+# get log from container
+date=$(date +"%F")
+for c in $CONTAINER; do
+    podman logs $c >> $date-report.log
+done
+
+
 # remove container
 for c in $CONTAINER; do
     echo "stopping container $c"
     podman stop $c
 done
 
-echo "$(date +"%H:%M:%S") :: all done"
+echo "$(date +"%T") :: all done"
 exit 0
